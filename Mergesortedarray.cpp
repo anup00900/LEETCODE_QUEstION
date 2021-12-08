488fd874816568e5d4ad6dd6f365ffe267717836
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2021-12-08 14:14:12 +0530

    Merge_Sorted Array

---
+++ Mergesortedarray.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
+        int i=m-1;
+        int j=n-1;
+        int p=m+n-1;
+        if(n==0)
+        {
+            return;
+        }
+        
+        
+        
+        while(i>=0 && j>=0)
+        {
+            
+            if(nums1[i]<nums2[j])
+            {
+                nums1[p]=nums2[j];
+                j--;
+                p--;
+            }
+            else if(nums1[i]>=nums2[j])
+            {
+                nums1[p]=nums1[i];
+                i--;
+                p--;
+            }
+           
+            
+        }
+        
+        if(i==-1)
+        {
+            while(j>=0)
+            {
+                nums1[p--]=nums2[j--];
+            }
+        }
+        
+        
+        
+        
+    }
+};
