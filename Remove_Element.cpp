383b3a51cf1985cf00ba3f615cc35f92b738a875
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2021-12-09 14:51:55 +0530

    Remove Element
    https://leetcode.com/problems/remove-element/

---
+++ Remove_Element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+         
+        
+        int i=0;
+        int j=nums.size()-1;
+        
+        while(i<=j)
+        {
+            if(val != nums[i])
+            {
+                i++;
+            }
+            else
+            {
+                
+                swap(nums[i],nums[j]);
+                j--;
+                
+            }
+        }   
+      
+        return(j+1);
+        
+    }
+};
