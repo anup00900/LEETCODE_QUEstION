60b5514d83dfab9dc9864a322a1ecea60ff066f3
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2022-03-26 06:08:38 +0530

    704. Binary Search

---
+++ Binaryseach.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int h=nums.size()-1;
+        int l=0;
+        while(l<=h)
+        {
+            int m=l+(h-l)/2;
+            if(nums[m]==target)
+            {
+                return m;
+            }
+            else if(nums[m]<target)
+            {
+                l=m+1;
+               
+            }
+            else
+            {
+                 h=m-1;
+            }
+        }
+        return -1;
+        
+    }
+};
