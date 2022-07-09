8859b8255cbd634ef46a3a9031ae04eb3ae37b6a
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2022-07-09 21:58:20 +0530

    386. Lexicographical Numbers

---
+++ LexicographicalNumbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        l=[]
+        for i in range(1,n+1):
+            l.append(str(i))
+        l.sort()
+        print(l)
+        return l
+            
+        
