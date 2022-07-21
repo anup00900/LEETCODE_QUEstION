6fc995c6f821d48bd0ab3b4bc655f399233eb673
ANUP ROY <75533606+anup00900@users.noreply.github.com>
2022-07-21 21:39:58 +0530

    35. Search Insert Position

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        
+        int l=0;
+        int h=nums.size()-1;
+        int m;
+        while(l<=h)
+        {
+            m = l+(h-l)/2;
+            
+            if(nums[m] == target)
+            {
+                return m;
+            }
+            else if(nums[m]>target)
+            {
+                    h=m-1;
+            }
+            else
+            {
+                l=m+1;
+            }
+        }
+        cout<<m<<" ";
+        
+        if(nums[m] > target)
+        {
+            return m;
+        }
+        else if(nums[m]<target)
+        {
+            return m+1;
+        }
+        return 0;
+        
+    }
+};
